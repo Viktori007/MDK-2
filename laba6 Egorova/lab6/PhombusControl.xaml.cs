48c3085a0a2a1798ec5d240b383b59bf61608fbe
Егорова Виктория <118374142+Viktori007@users.noreply.github.com>
2023-02-09 21:53:57 +0300

    Add files via upload

---
+++ laba6 Egorova/lab6/PhombusControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6
+{
+    /// <summary>
+    /// Логика взаимодействия для PhombusControl.xaml
+    /// </summary>
+    public partial class PhombusControl : UserControl
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource ColoredSource { get; set; }
+        public ImageSource BlackWhiteSource =>
+            (ColoredSource == null) ? null : new FormatConvertedBitmap((BitmapSource)ColoredSource, PixelFormats.Gray8, null, 0);
+
+        public PhombusControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnAction_Click(object senter, RoutedEventArgs e)
+        {
+          MessageBox.Show(Description, Header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
